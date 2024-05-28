5dc20e7ab77b471f1b02e3e46a35848e5c108b01
jagat mettra chakma <mettradbd@gmail.com>
2024-05-28 21:29:57 +0600

    chart midify

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -40,7 +40,7 @@ const Statistics = () => {
                 </div>
             </section>
             {/* chart two */}
-            <section>
+            {/* <section>
                 <ResponsiveContainer width="100%" height="100%">
                     <PieChart width={400} height={400}>
                         <Pie
@@ -60,7 +60,7 @@ const Statistics = () => {
                     </PieChart>
                 </ResponsiveContainer>
 
-            </section>
+            </section> */}
         </div>
     );
 };
